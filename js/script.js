6c0dad06c6b0759485511116afc903408aca18a4
lfrox <luizfelipergs88@gmail.com>
2018-06-22 04:45:19 -0300

    galeria nova

--- js/script.js
+++ js/script.js
@@ -11,9 +11,37 @@ menu.addEventListener("click", () => {
   setacima.classList.toggle("hide")
 });
 
-
 // $(".menu").click(function() { // function() {} === () => {}
 //   $(this).toggleClass("hide show")
 //   $(".setacima").toggleClass("hide show")
 //   $(".setabaixo").toggleClass("hide show")
 // });
+
+$('[data-fancybox]').fancybox({
+  keyboard: true,
+  loop: true,
+  protect: true,
+  buttons : [
+    "zoom",
+    "share",
+    "slideShow",
+    "fullScreen",
+    "thumbs",
+    "close"
+  ],
+  animationEffect: "zoom",
+  transitionEffect: "zoom-in-out",
+  transitionDuration: 466,
+});
+
+
+$('[data-fancybox="watermark"]').fancybox({
+  protect    : true,
+  slideClass : 'watermark',
+  toolbar    : false,
+  smallBtn   : true
+});
+
+// Preload watermark image
+// Please, use your own image
+(new Image()).src = "https://fancyapps.com/GJbkSPU.png";
